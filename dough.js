9cbbdb3d0430892a1d49aa3d899c1970f3e7a84f
Nathan Searles <nsearles@gmail.com>
2011-06-14 16:22:37 -0700

    1.3: 	Fixed: issue with JSON detection

--- dough.js
+++ dough.js
@@ -2,8 +2,8 @@
 * Dough, A Cookie Plugin for jQuery
 * By: Nathan Searles, http://nathansearles.com
 * Example: http://nathansearles.com/plugin/dough
-* Version: 1.2
-* Updated: April 21th, 2011
+* Version: 1.3
+* Updated: June 14th, 2011
 *
 * Licensed under the Apache License, Version 2.0 (the 'License');
 * you may not use this file except in compliance with the License.
@@ -20,30 +20,30 @@
 	
 /*
 * Create cookie
-*		$.dough('cookieName', 'cookieValue');
+*		$.dough("cookieName", "cookieValue");
 *
 * Read cookie
-*		$.dough('cookieName');
+*		$.dough("cookieName");
 *
 * Delete cookie
-*		$.dough('cookieName', 'remove');
+*		$.dough("cookieName", "remove");
 *	
-*	Note: If setting the path to 'current', you must also define that when removing the cookie.
-*		$.dough('cookieName', 'remove', { path: 'current' });
+*	Note: If setting the path to "current", you must also define that when removing the cookie.
+*		$.dough("cookieName", "remove", { path: "current" });
 *	
 * Set full cookie
-*		$.dough('cookieName', 'cookieValue', { expires: 365, path: 'current', domain: 'auto', secure: true });
+*		$.dough("cookieName", "cookieValue", { expires: 365, path: "current", domain: "auto", secure: true });
 *
-*	Example cookie has a name of 'cookieName', a value of 'cookieValue', will expire in 1 year, have path of current page, domain will be autodetected and is set to secure for a use under https://.
+*	Example cookie has a name of "cookieName", a value of "cookieValue", will expire in 1 year, have path of current page, domain will be autodetected and is set to secure for a use under https://.
 *
 * Definition
 *	$.dough([name], [value], { [options] });
 *
 * Options Definition
-*	expires: Days 'til cookie expires
-*	path: Default is root '/', set to 'current' to use the path of current page
+*	expires: Days "til cookie expires
+*	path: Default is root "/", set to "current" to use the path of current page
 *	domain: Auto detect and set domain with subdomain prefix
-*	secure: Set to true if you're using https://
+*	secure: Set to true if you"re using https://
 */
 		
 (function($){
@@ -51,14 +51,14 @@
 		// Override options if specified
 		option = $.extend( {}, $.dough.option, option );
 		
-		// We'll need this later on
+		// We"ll need this later on
 		var days = option.expires;
-		
+
 		// Get cookie value
-		if (value === undefined) {
-			var results = document.cookie.match ( name + '=([^;]*)(;|$)' );
-			if (results) {
-				if (results[1][0] == '[') {
+		if (typeof(value) === "undefined") {
+			var results = document.cookie.match( name + "=([^;]*)(;|$)" );
+			if ( results[1] ) {
+				if ( results[1][0] === "{" ) {
 					// Parse as JSON
 					return $.parseJSON(results[1])[0];
 				} else {
@@ -70,48 +70,48 @@
 		}
 		
 		// Remove cookie
-		if (value == 'remove') {
+		if (value == "remove") {
 			days = -1;
-			value = '';
+			value = "";
 		}
 
 		// Start the baking
-		var bake = name + '=' + value;
+		var bake = name + "=" + value;
 
 		// Set the date
 		if (days) {
 			var date = new Date();
-			// Get current date add/remove days 'til expired
+			// Get current date add/remove days "til expired
 			date.setTime(date.getTime()+(days*24*60*60*1000));
 			// Convert to UTC string.
-			bake += '; expires=' + date.toUTCString();
+			bake += "; expires=" + date.toUTCString();
 		}
 
 		// Set the path
-		if (option.path == 'current') {
-			bake += ';';
+		if (option.path == "current") {
+			bake += ";";
 		} else {
-			bake += '; path=' + option.path + ';';
+			bake += "; path=" + option.path + ";";
 		}
 		
 		// Set the domain
 		if (option.domain) {
-			if (option.domain == 'auto') {
+			if (option.domain == "auto") {
 				// Get domain name or IP address. Returns 192.168.1.1 or .example.com for use with subdomains
 				var ipAddress = window.location.hostname.match(/\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b/g),
-					domainName = '.' + window.location.hostname;
+					domainName = "." + window.location.hostname;
 				// Is locaiton an IP address or domain name?
 				var currentLocation = ipAddress ? ipAddress.toString() : domainName.toString();
-				bake += '; domain=' + currentLocation + ';';
+				bake += "; domain=" + currentLocation + ";";
 			} else {
 				// If domain specified
-				bake += '; domain=' +  option.domain + ';';
+				bake += "; domain=" +  option.domain + ";";
 			}
 		}
 		
 		// If secure connection required
 		if (option.secure) {
-			bake += '; secure';
+			bake += "; secure";
 		}
 
 		// Cookie is baked.
@@ -121,7 +121,7 @@
 	// default options
 	$.dough.option = {
 		expires: false,
-		path: '/',
+		path: "/",
 		domain: false,
 		secure: false
 	};
